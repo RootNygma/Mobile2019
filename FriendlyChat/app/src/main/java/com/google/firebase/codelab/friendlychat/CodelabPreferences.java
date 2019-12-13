dc30113aab0052dd0283ff5c19982ecb0aee487f
Nygma <nygma@pop-os.localdomain>
2019-12-13 03:56:45 +0000

    friendly chat

---
+++ FriendlyChat/app/src/main/java/com/google/firebase/codelab/friendlychat/CodelabPreferences.java
@@ -0,0 +1,8 @@
+
+package com.google.firebase.codelab.friendlychat;
+
+public class CodelabPreferences {
+
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+
+}
---
+++ FriendlyChat/app/src/main/java/com/google/firebase/codelab/friendlychat/MyFirebaseMessagingService.java
@@ -0,0 +1,19 @@
+package com.google.firebase.codelab.friendlychat;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService {
+
+    private static final String TAG = "MyFMService";
+
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Handle data payload of FCM messages.
+        Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
+        Log.d(TAG, "FCM Notification Message: " + remoteMessage.getNotification());
+        Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
+    }
+}
